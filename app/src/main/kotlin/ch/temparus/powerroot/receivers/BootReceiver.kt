2ed5927e889432bf801bbb99de91e322f261e75b
Sandro Lutz <sandro.lutz@temparus.ch>
2018-01-21 12:47:03 +0100

    Check if device is plugged in on system boot

--- app/src/main/kotlin/ch/temparus/powerroot/receivers/BootReceiver.kt
+++ app/src/main/kotlin/ch/temparus/powerroot/receivers/BootReceiver.kt
@@ -3,12 +3,13 @@ package ch.temparus.powerroot.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import ch.temparus.powerroot.SharedMethods
 import ch.temparus.powerroot.services.BatteryService
 
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action && SharedMethods.isDevicePluggedIn(context)) {
             BatteryService.start(context)
         }
     }
